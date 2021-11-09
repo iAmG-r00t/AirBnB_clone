fb8782fdaf62f6cbbb410bf0c773a4013a4ca7f7
Jtobyy <jtobi8161@gmail.com>
2021-11-09 17:33:51 +0000

    BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -3,22 +3,39 @@
 Defines the base model
 """
 import uuid
-import datetime
+from datetime import datetime
+
 
 class BaseModel:
     """
     Defines all common attributes and methods for other classes
     """
     def __init__(self):
+        """
+        Initializes an instance
+        """
         self.id = str(uuid.uuid4())
-        created_at = datetime.now()
-        updated_at = datetime.now()
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def __str__(self):
-        return f"[{self.__name__}] ({self.id}) <{self.__dict__}>"
+        """
+        String representation when instance is printed
+        """
+        return f"[{type(self).__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
+        """
+        Save updates to an instance
+        """
         self.updated_at = datetime.now()
 
     def to_dict(self):
-        
+        """
+        Returns a dictionary representation of an instance
+        """
+        self.__dict__.update({"__class__": type(self).__name__,
+                              "updated_at": self.updated_at.isoformat(),
+                              "id": self.id,
+                              "created_at": self.created_at.isoformat()})
+        return self.__dict__
---
+++ tests/test_base_model.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Test suite for base_model
+"""
+import unittest
+from models.basemodel import BaseModel
+
+
+class TestBaseModel(unittest.TestCase):
+    
