44152da78209f6c22b96bffcd222c1b0e2e9e1b2
Jtobyy <jtobi8161@gmail.com>
2021-11-15 00:57:47 +0000

    More Unittest cases for base_model.py

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -10,3 +10,7 @@ class TestBaseModel(unittest.TestCase):
     def test_str(self):
         base = BaseModel()
         self.assertEqual(base.__str__(), f"[{type(base).__name__}] ({base.id}) {base.__dict__}")
+
+    def test_to_dict(self):
+        base = BaseModel()
+        self.assertDictEqual(base.to_dict(), {'__class__': type(base).__name__, 'updated_at': base.updated_at.isoformat(), 'id': base.id, 'created_at': base.created_at.isoformat()})
